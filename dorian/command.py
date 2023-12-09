e864e9083c3b160b6ca9aa781b87a91283453f22
Uttam Kini <uttamkini@Uttams-MacBook-Pro.local>
2023-12-09 15:18:47 +0530

    Fail run_command even if one command fails

--- dorian/command.py
+++ dorian/command.py
@@ -3,7 +3,5 @@ import subprocess
 
 
 def run_command(cmd: str):
+    cmd = f"""set -e; {cmd}"""
     return subprocess.check_output(cmd, shell=True, stderr=subprocess.STDOUT)
-    # return_code = os.system(cmd)
-    # if return_code != 0:
-    #     raise Exception("Setup failed")
