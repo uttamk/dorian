b120f715e2f0c80d4a5b6b405f2a1be6e6034497
Uttam Kini <uttamkini@Uttams-MacBook-Pro.local>
2024-01-06 09:51:34 +0530

    Refactor tests

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -38,7 +38,7 @@ def test_with_one_deployment():
         git.commit(batch_1_start_time + timedelta(hours=1), f"Batch 1 commit 2")
         git.commit(batch_1_start_time + timedelta(hours=2), f"Batch 1 commit 3")
         sha = git.commit(deploy_1_time, f"Batch 1 final commit")
-        git.create_tag(f'deploy-{deploy_1_time.strftime("%Y%m%d%H%M%S")}', sha)
+        git.create_tag(_deploy_tag(deploy_1_time), sha)
 
         runner = CliRunner()
         result = runner.invoke(cli.cli, ["git@github.com:uttamk/dorian.git"])
@@ -49,6 +49,10 @@ def test_with_one_deployment():
         assert list(csv.DictReader(f)) == [
             dict(deployment_time=str(deploy_1_time.timestamp()), first_commit_time=''),
         ]
+
+
+def _deploy_tag(deploy_datetime):
+    return f'deploy-{deploy_datetime.strftime("%Y%m%d%H%M%S")}'
 
 
 def test_with_two_deployments():
@@ -65,11 +69,11 @@ def test_with_two_deployments():
         git.commit(batch_1_start_time + timedelta(hours=1), f"Batch 1 commit 2")
         git.commit(batch_1_start_time + timedelta(hours=2), f"Batch 1 commit 3")
         sha = git.commit(deploy_1_time, f"Batch 1 final commit")
-        git.create_tag(f'deploy-{deploy_1_time.strftime("%Y%m%d%H%M%S")}', sha)
+        git.create_tag(_deploy_tag(deploy_1_time), sha)
         git.commit(batch_2_start_time, f"Batch 2 commit 1")
         git.commit(deploy_1_time + timedelta(hours=2), f"Batch 2 commit 2")
         sha = git.commit(deploy_2_time, f"Batch 2 final commit")
-        git.create_tag(f'deploy-{deploy_2_time.strftime("%Y%m%d%H%M%S")}', sha)
+        git.create_tag(_deploy_tag(deploy_2_time), sha)
 
         runner = CliRunner()
         result = runner.invoke(cli.cli, ["git@github.com:uttamk/dorian.git"])
@@ -101,15 +105,15 @@ def test_with_rollback():
         git.commit(batch_1_start_time + timedelta(hours=1), f"Batch 1 commit 2")
         git.commit(batch_1_start_time + timedelta(hours=2), f"Batch 1 commit 3")
         deploy_1_sha = git.commit(deploy_1_time, f"Batch 1 final commit")
-        git.create_tag(f'deploy-{deploy_1_time.strftime("%Y%m%d%H%M%S")}', deploy_1_sha)
+        git.create_tag(_deploy_tag(deploy_1_time), deploy_1_sha)
         git.commit(batch_2_start_time, f"Batch 2 commit 1")
         git.commit(deploy_1_time + timedelta(hours=2), f"Batch 2 commit 2")
         sha = git.commit(deploy_2_time, f"Batch 2 final commit")
-        git.create_tag(f'deploy-{deploy_2_time.strftime("%Y%m%d%H%M%S")}', sha)
-        git.create_tag(f'deploy-{rollback_time.strftime("%Y%m%d%H%M%S")}', deploy_1_sha)
+        git.create_tag(_deploy_tag(deploy_2_time), sha)
+        git.create_tag(_deploy_tag(rollback_time), deploy_1_sha)
         git.commit(batch_3_start_time + timedelta(hours=1), f"Batch 3 commit 2")
         sha = git.commit(batch_3_start_time + timedelta(hours=2), f"Batch 3 commit 3")
-        git.create_tag(f'deploy-{deploy_3_time.strftime("%Y%m%d%H%M%S")}', sha)
+        git.create_tag(_deploy_tag(deploy_3_time), sha)
 
         runner = CliRunner()
         result = runner.invoke(cli.cli, ["git@github.com:uttamk/dorian.git"])
